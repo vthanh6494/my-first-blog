3f1dc737c14b1c59d146de32c2fee73ece494725
Vo Thi Hong Anh <46419913+vthanh6494@users.noreply.github.com>
2019-05-12 14:05:40 +0700

    update post_list

--- blog/forms.py
+++ blog/forms.py
@@ -6,7 +6,7 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'text',)
+        fields = ('title', 'text', 'text2')
 
 class CommentForm(forms.ModelForm):
     

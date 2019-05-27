e12ef68c15d17c1fc97fa8d88bac636c9c61aa28
Vo Thi Hong Anh <honganhftu01677@gmail.com>
2019-05-27 14:21:01 +0700

    update comment control

--- blog/models.py
+++ blog/models.py
@@ -33,7 +33,7 @@ class Comment(models.Model):
     author = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    approved_comment = models.BooleanField(default=True)
+    approved_comment = models.BooleanField(default=False) #True
 
     def approve(self):
         self.approved_comment = True

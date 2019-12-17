04cda5af3b77bd539ae2728fa6db3121b92b1999
vthanh6494 <honganhftu01677@gmail.com>
2019-12-17 13:47:32 +0700

    update database

--- mysite/settings.py
+++ mysite/settings.py
@@ -113,8 +113,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangogirls',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 

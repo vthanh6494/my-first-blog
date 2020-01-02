a805afc666f7367837f7a57a2c6056e70673b5f3
vthanh6494 <honganhftu01677@gmail.com>
2020-01-02 15:30:10 +0700

    Revert "update database"
    This reverts commit 04cda5af3b77bd539ae2728fa6db3121b92b1999.

--- mysite/settings.py
+++ mysite/settings.py
@@ -113,12 +113,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangogirls',
-        'USER': 'name',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 

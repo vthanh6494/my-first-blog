1d0173ed206def8b94b749140726f41fd32973f9
vthanh6494 <honganhftu01677@gmail.com>
2020-01-08 17:16:06 +0700

    fixed menu

--- blog/static/js/blog.js
+++ blog/static/js/blog.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
     $(window).load(function(){
         var socialIcons = $('.social-icons').offset().top;
+        var htmlAdded1  = '<div class="wrap-logo"><a class="added-info" href="/"><img src="/static/images/python-logo.png" alt="python-logo"></img></a><p>Anhcun blog</p></div>';
+        var htmltAdded2 = '<div class="social-icons"><a class="cv-icon" href="/resume"><img src="/static/images/cv-icon.png" alt="icon-CV"></a><a href="https://www.facebook.com/akiko.vo94"><i class="fab fa-facebook-f"></i></a><a href="https://www.instagram.com/honganhftu01677/"><i class="fab fa-instagram"></i></a><a href="https://github.com/vthanh6494?tab=repositories"><i class="fab fa-github"></i></a></div>';
         $(this).scroll(function () {
             if ($(window).scrollTop() >= socialIcons) {
-                $('.social-icons').addClass("fixed-icons-bar");
+                $('.show-sp').addClass("fixed-icons-bar");
+                $('.show-sp').html(htmlAdded1+htmltAdded2);
                 $('.container').addClass("plus-margin-top");
             } else {
-                $('.social-icons').removeClass("fixed-icons-bar");
+                $('.show-sp').removeClass("fixed-icons-bar");
                 $('.container').removeClass("plus-margin-top");
+                $('.show-sp').html(htmltAdded2);
             }
         });
     });
